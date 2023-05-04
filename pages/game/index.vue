90be2c03040fbe19193efd52a4339c1e0979a673
MadHeo <tnlfl288@gmail.com>
2023-05-04 17:22:52 +0900

    feat: 숫자야구게임 만들기

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -1,29 +1,12 @@
 <template>
-  <button class="border bg-red-200 p-5" @click="show = !show">클릭</button>
-  <div class="w-screen h-screen flex justify-center items-center">
-    <transition name="imgAnim">
-      <img v-if="show" src="/webb1.jpg" alt="" />
-    </transition>
-  </div>
+  <numberBaseball />
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      show: true,
-    };
-  },
-};
-</script>
+<script></script>
 
 <style>
 .imgAnim-enter-active,
 .imgAnim-leave-active {
   transition: opacity 0.5s ease;
-}
-.imgAnim-enter-from,
-.imgAnim-leave-to {
-  opacity: 0;
 }
 </style>
